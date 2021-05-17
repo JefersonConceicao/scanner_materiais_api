cb5b34ea9caaa31568994445f5a6beb9dfe6b7b7
getibt <geti@bahiatursa.ba.gov.br>
2021-05-17 14:54:44 -0300

    setting up npm build

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -19,7 +19,7 @@
         <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/AdminLTE.min.css') }}"/>
         @yield('adminlte_css')
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <link rel="stylesheet" href="{{asset('css/app.min.css')}}">
     </head>
 
     <body class="hold-transition @yield('body_class') layout-boxed">
@@ -36,6 +36,6 @@
         @include('adminlte::plugins', ['type' => 'js'])
         @yield('adminlte_js')
         <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/i18n/pt-BR.js">  </script>
-        <script src="{{ asset('js/app.js')}}"> </script>
+        <script src="{{ asset('js/app.min.js')}}"> </script>
     </body>
 </html>
