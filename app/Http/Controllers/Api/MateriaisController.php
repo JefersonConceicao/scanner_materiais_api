98a1b6d8e706ab661b975c6ae430ab02614b2734
Jeferson Conceicao <jefersonmallone2000@outlook.com>
2021-08-23 11:23:27 -0300

    push prod

--- app/Http/Controllers/Api/MateriaisController.php
+++ app/Http/Controllers/Api/MateriaisController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 
 //MODEL
 use App\Models\Materiais;
+use App\Models\Patrimonio;
 
 class MateriaisController extends Controller
 {
@@ -14,10 +15,11 @@ class MateriaisController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function scanner(Request $request)
-    {
+    {   
+        $patrimonio = new Patrimonio;
         $materiais = new Materiais;
 
-        $data = $materiais->coletaMateriais($request->all());
+        $data = $patrimonio->getPatrimonioByCodigo($request->codigo_barra);
         return response()->json($data);
     }
 
