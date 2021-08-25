91b35dcbc44929f422df037c50cf5d3630361e68
Jeferson Conceicao <jefersonmallone2000@outlook.com>
2021-08-25 16:55:41 -0300

    api patrimonio

--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,4 @@
 Route::get('/setores', 'Api\SetoresController@index')->name('setores.index');
 Route::get('/listMateriais/{setorId}', 'Api\MateriaisController@list')->name('materiais.list');
 Route::get('/scanner', 'Api\MateriaisController@scanner')->name('materiais.scanner');
+Route::post('/saveMateriais', 'Api\MateriaisController@store')->name('materiais.store');
